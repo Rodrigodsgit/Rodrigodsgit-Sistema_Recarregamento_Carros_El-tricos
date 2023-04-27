1bc020d6a328b37acf14c206775013420022f9e8
Paulo Queiroz <paulocarvalho.pq@gmail.com>
2023-04-26 22:01:10 -0300

    style: Print da coordenada arredondado

--- Car/CarSystem.py
+++ Car/CarSystem.py
@@ -67,11 +67,11 @@ def carInMoviment(car):
 
     while (True):
         destiny = [random.uniform(-12.285, -12.205), random.uniform(-38.990, -38.905)]
-        print(f"Indo para {destiny}")
+        print(f"Indo para [{round(destiny[0], 4)},{round(destiny[1], 4)}]")
         while (car.latitude != destiny[0] and car.longitude != destiny[1]):
             time.sleep(10)
             car.updateLocation(0.01,destiny)
-            print(f"Carro esta em [{car.latitude},{car.longitude}]")
+            print(f"Carro esta em [{round(car.latitude, 4)},{round(car.longitude, 4)}]")
 
 def printCarBattery(car):
     while (True):
